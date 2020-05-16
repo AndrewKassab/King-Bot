a5a70a7acfff83c9d6aa3445a6ddf264c0ab9e0c
akassab <andrewkassab@live.com>
2020-05-16 16:05:35 -0700

    changed to once every 5 messages

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 👑 King Bot 👑
 
-Reacts to messages sent by the server owner with a crown emoji once per 3 minutes. Let all your underlings know who is boss!
+Reacts to messages sent by the server owner with a crown emoji once per 5 messages. Let all your underlings know who is boss!
 
 [**CLICK HERE ADD TO YOUR SERVER**](https://discord.com/api/oauth2/authorize?client_id=711316876738691163&permissions=0&scope=bot)
--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from discord.ext import commands
 
 token = os.getenv('KING_TOKEN')
 
+every_num_messages = 5
+
 # id of user to be crowned 
 emoji = '👑'
 
@@ -15,11 +17,16 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+    reaction_counter = 0
+    with open('reaction_counter.txt', 'r') as data_file:
+        reaction_counter = int(data_file.read())
     king_id = message.guild.owner_id
     if king_id == message.author.id:
-        await message.add_reaction(emoji)
-        time.sleep(180)
+        if reaction_counter % every_num_messages == 0:
+            await message.add_reaction(emoji)
+        with open('reaction_counter.txt', 'w') as data_file:
+            reaction_counter += 1
+            data_file.write(reaction_counter)
 
 
 bot.run(token)
-
