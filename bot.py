b88abc92efa9a5997095267c50f127de94ffd44e
akassab <andrewkassab@live.com>
2020-05-16 15:33:26 -0700

    bot complete

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from discord.ext import commands
+
+token = os.getenv('KING_TOKEN')
+
+# id of user to be crowned 
+emoji = '👑'
+
+bot = commands.Bot(".")
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to discord!')
+
+@bot.event
+async def on_message(message):
+    king_id = message.guild.owner_id
+    if king_id == message.author.id:
+        await message.add_reaction(emoji)
+
+bot.run(token)
+
